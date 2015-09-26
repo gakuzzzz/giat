d43f1202baa7a4936f778ce51edb03e2c27fd52d
gakuzzzz <gaku_github@t2v.jp>
2015-09-26 20:32:48 +0900

    Added Kleisli instances

--- src/main/scala/giat/data/Function1.scala
+++ src/main/scala/giat/data/Function1.scala
@@ -22,13 +22,52 @@ trait Function1Definition {
 
 trait Function1Instances {
 
-  import Monad.ops._
+  protected trait KleisliFunctor[F[_], E] extends Functor[Kleisli[F, E, ?]] {
+    implicit def F: Functor[F]
+    override def map[A, B](fa: Kleisli[F, E, A])(f: A => B): Kleisli[F, E, B] = { e => F.map(fa(e))(f) }
+  }
 
-  protected def kleisliMonad[F[_]: Monad, E]: Monad[Kleisli[F, E, ?]] = new Monad[Kleisli[F, E, ?]] {
-    override def point[A](a: => A): Kleisli[F, E, A] = _ => Monad[F].point(a)
-    override def flatMap[A, B](fa: Kleisli[F, E, A])(f: A => Kleisli[F, E, B]): Kleisli[F, E, B] = { e =>
-      fa(e).flatMap { a: A => f(a)(e) }
+  protected trait KleisliApply[F[_], E] extends Apply[Kleisli[F, E, ?]] with KleisliFunctor[F, E] {
+    override implicit def F: Apply[F]
+    override def ap[A, B](fa: => Kleisli[F, E, A])(f: => Kleisli[F, E, A => B]): Kleisli[F, E, B] = { e =>
+      F.ap(fa(e))(f(e))
     }
+  }
+
+  protected trait KleisliApplicative[F[_], E] extends Applicative[Kleisli[F, E, ?]] with KleisliApply[F, E] {
+    override implicit def F: Applicative[F]
+    override def point[A](a: => A): Kleisli[F, E, A] = { e => F.point(a) }
+  }
+
+  protected trait KleisliBind[F[_], E] extends Bind[Kleisli[F, E, ?]] with KleisliApply[F, E] {
+    override implicit def F: Bind[F]
+    override def flatMap[A, B](fa: Kleisli[F, E, A])(f: A => Kleisli[F, E, B]): Kleisli[F, E, B] = { e =>
+      F.flatMap(fa(e)) { f(_)(e) }
+    }
+  }
+
+  protected trait KleisliMonad[F[_], E] extends Monad[Kleisli[F, E, ?]] with KleisliApplicative[F, E] with KleisliBind[F, E] {
+    override implicit def F: Monad[F]
+  }
+
+  protected def kleisliFunctor[F[_], E](implicit mf: Functor[F]): Functor[Kleisli[F, E, ?]] = new KleisliFunctor[F, E] {
+    override implicit def F = mf
+  }
+
+  protected def kleisliApply[F[_], E](implicit mf: Apply[F]): Apply[Kleisli[F, E, ?]] = new KleisliApply[F, E] {
+    override implicit def F = mf
+  }
+
+  protected def kleisliApplicative[F[_], E](implicit mf: Applicative[F]): Applicative[Kleisli[F, E, ?]] = new KleisliApplicative[F, E] {
+    override implicit def F = mf
+  }
+
+  protected def kleisliBind[F[_], E](implicit mf: Bind[F]): Bind[Kleisli[F, E, ?]] = new KleisliBind[F, E] {
+    override implicit def F = mf
+  }
+
+  protected def kleisliMonad[F[_], E](implicit mf: Monad[F]): Monad[Kleisli[F, E, ?]] = new KleisliMonad[F, E] {
+    override implicit def F = mf
   }
 
   protected def stateMonad[S]: Monad[State[S, ?]] = new Monad[State[S, ?]] {
@@ -51,23 +90,33 @@ object StdFunction1 extends Function1Instances { self =>
   import Id.monad.default
 
   object monad {
-    implicit def reader[E]: Monad[Reader[E, ?]] = self.kleisliMonad[Id, E]
+    implicit def kleisli[F[_]: Monad, E]: Monad[Kleisli[F, E, ?]] = self.kleisliMonad[F, E]
+    implicit def readerT[F[_]: Monad, E]: Monad[ReaderT[F, E, ?]] = self.kleisliMonad[F, E]
+    implicit def reader[E]: Monad[Reader[E, ?]] = kleisli[Id, E]
     implicit def state[S]: Monad[State[S, ?]] = self.stateMonad[S]
   }
   object bind {
-    implicit def reader[E]: Bind[Reader[E, ?]] = self.kleisliMonad[Id, E]
+    implicit def kleisli[F[_]: Bind, E]: Bind[Kleisli[F, E, ?]] = self.kleisliBind[F, E]
+    implicit def readerT[F[_]: Bind, E]: Bind[ReaderT[F, E, ?]] = self.kleisliBind[F, E]
+    implicit def reader[E]: Bind[Reader[E, ?]] = kleisli[Id, E]
     implicit def state[S]: Bind[State[S, ?]] = self.stateMonad[S]
   }
   object applicative {
-    implicit def reader[E]: Applicative[Reader[E, ?]] = self.kleisliMonad[Id, E]
+    implicit def kleisli[F[_]: Applicative, E]: Applicative[Kleisli[F, E, ?]] = self.kleisliApplicative[F, E]
+    implicit def readerT[F[_]: Applicative, E]: Applicative[ReaderT[F, E, ?]] = self.kleisliApplicative[F, E]
+    implicit def reader[E]: Applicative[Reader[E, ?]] = kleisli[Id, E]
     implicit def state[S]: Applicative[State[S, ?]] = self.stateMonad[S]
   }
   object apply {
-    implicit def reader[E]: Apply[Reader[E, ?]] = self.kleisliMonad[Id, E]
+    implicit def kleisli[F[_]: Apply, E]: Apply[Kleisli[F, E, ?]] = self.kleisliApply[F, E]
+    implicit def readerT[F[_]: Apply, E]: Apply[ReaderT[F, E, ?]] = self.kleisliApply[F, E]
+    implicit def reader[E]: Apply[Reader[E, ?]] = kleisli[Id, E]
     implicit def state[S]: Apply[State[S, ?]] = self.stateMonad[S]
   }
   object functor {
-    implicit def reader[E]: Functor[Reader[E, ?]] = self.kleisliMonad[Id, E]
+    implicit def kleisli[F[_]: Functor, E]: Functor[Kleisli[F, E, ?]] = self.kleisliFunctor[F, E]
+    implicit def readerT[F[_]: Functor, E]: Functor[ReaderT[F, E, ?]] = self.kleisliFunctor[F, E]
+    implicit def reader[E]: Functor[Reader[E, ?]] = kleisli[Id, E]
     implicit def state[S]: Functor[State[S, ?]] = self.stateMonad[S]
   }
 
